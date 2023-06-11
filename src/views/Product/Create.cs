8262e8be694fd97010e01d1089e08694f67d24ef
Gabriel <bielsobv@gmail.com>
2023-06-10 23:04:55 -0300

    Command implementation on project

---
+++ src/views/Product/Create.cs
@@ -0,0 +1,105 @@
+using Controllers;
+using Models;
+
+namespace Views
+{
+    public class Create : Form
+    {
+        public Label lblId;
+        public TextBox txtId;
+        public Label lblName;
+        public TextBox txtName;
+        public Label lblValue;
+        public TextBox txtValue;
+        public Button btCrt;
+
+        public void btCrt_Click(object sender, EventArgs e)
+        {
+            if(
+                txtName.Text == "" ||
+                txtValue.Text == ""
+            ){
+                MessageBox.Show("Fill out the fields correctly.");
+                return;
+            }else{
+                Models.Product product = new Models.Product
+                (
+                    Convert.ToInt32(txtId),
+                    txtName.Text,
+                    Convert.ToDouble(txtValue.Text) 
+                );
+
+                Controllers.Product.Store(product);
+
+                MessageBox.Show("Product registered successfully.");
+                ClearForm();
+            }
+
+            List Product = Application.OpenForms.OfType<List>().FirstOrDefault();
+            if(Product != null)
+            {
+                Product.RefreshList();
+            }
+            this.Close();
+        }
+
+        private void ClearForm()
+        {
+            txtId.Clear();
+            txtName.Clear();
+            txtValue.Clear();
+        }
+
+        public Create()
+        {
+            this.Text = "Register a product";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Size = new System.Drawing.Size(280, 360);
+
+            this.lblId = new Label();
+            this.lblId.Text = "Id";
+            this.lblId.Location = new Point(10, 40);
+            this.lblId.Size = new Size(50, 20);
+
+            this.txtId = new TextBox();
+            this.txtId.Location = new Point(80 ,40);
+            this.txtId.Size = new Size(150, 20);
+
+            this.lblName = new Label();
+            this.lblName.Text = "Name";
+            this.lblName.Location = new Point(10, 70);
+            this.lblName.Size = new Size(50, 20);
+
+            this.txtName = new TextBox();
+            this.txtName.Location = new Point(80, 70);
+            this.txtName.Size = new Size(150, 20);
+
+            this.lblValue = new Label();
+            this.lblValue.Text = "Value";
+            this.lblValue.Location = new Point(10, 100);
+            this.lblValue.Size = new Size(50, 20);
+
+            this.txtValue = new TextBox();
+            this.txtValue.Location = new Point(80, 100);
+            this.txtValue.Size = new Size(150, 20);
+
+            this.btCrt = new Button();
+            this.btCrt.Text = "Add";
+            this.btCrt.Location = new Point(10, 150);
+            this.btCrt.Size = new Size(50, 20);
+
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btCrt);
+        }
+    }
+}
---
+++ src/views/Product/Product.cs
@@ -0,0 +1,153 @@
+using Models;
+using Controllers;
+
+namespace Views
+{
+    public class List : Form
+    {
+        public enum Option {Update, Delete}
+        ListView listProduct;
+
+        private void AddListView(Models.Product product)
+        {
+            string[]row = {
+                product.Id.ToString(),
+                product.Name,
+                product.Value.ToString()
+            };
+
+            ListViewItem item = new ListViewItem(row);
+            listProduct.Items.Add(item);
+        }
+
+        public void RefreshList()
+        {
+            listProduct.Items.Clear();
+
+            List<Models.Product> list = Controllers.Product.Read();
+
+            foreach (Models.Product product in list)
+            {
+                AddListView(product);
+            }
+        }
+
+        private void btCrt_Click(object sender, EventArgs e)
+        {
+            var Create = new Views.Create();
+            Create.Show();
+        }
+
+        private void btUpd_Click(object sender, EventArgs e)
+        {
+            try{
+                
+                Models.Product product = GetSelectedProduct(Option.Update);
+                RefreshList();
+                var ProductUpdateView = new Views.Update(product);
+                if(ProductUpdateView.ShowDialog() == DialogResult.OK);
+                {
+                    RefreshList();
+                    MessageBox.Show("Product edited successfully.");
+                }
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            try{
+                Models.Product product = GetSelectedProduct(Option.Delete);
+                DialogResult result = MessageBox.Show("Do you really want to delete this product?" "Confirm deletion", MessageBoxButtons.YesNo);
+                if(result == DialogResult.Yes)
+                {
+                    Controllers.Product.destroy(product);
+                    RefreshList();
+                }
+            }
+            catch(Exception err)
+            {
+                if(err.InnerException != null)
+                {
+                    MessageBox.Show(err.InnerException.Message);
+                }
+                else
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+        }
+
+        public Models.Product GetSelectedProduct(Option option)
+        {
+            if(listProduct.SelectedItems.Count > 0)
+            {
+                int selectedProductId = int.Parse(listProduct.SelectedItems[0].Text);
+                return Controllers.Product.ReadById(selectedProductId);
+            }
+            else{
+                throw new Exception($"Select a Product for {(option == Option.Update ? "udpate" : "delete")}");
+            }
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public List()
+        {
+            this.Text = "Products";
+            this.Size = new Size(800, 450);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = true;
+            this.MinimizeBox = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+
+            listProduct = new ListView();
+            listProduct.Size = new Size(680, 260);
+            listProduct.Location = new Point(50, 50);
+            listProduct.View = View.Details;
+            listProduct.Columns.Add("Id");
+            listProduct.Columns.Add("Name");
+            listProduct.Columns.Add("Value");
+            listProduct.FullRowSelect = true;
+            this.Controls.Add(listProduct);
+
+            RefreshList();
+
+            Button btCrt = new Button();
+            btCrt.Text = "Add";
+            btCrt.Size = new Size(100, 30);
+            btCrt.Location = new Point(50, 330);
+            btCrt.Click += new EventHandler(btCrt_Click);
+            this.Controls.Add(btCrt);
+
+            Button btUpd = new Button();
+            btUpd.Text = "Update";
+            btUpd.Size = new Size(100, 30);
+            btUpd.Location = new Point(170, 330);
+            btUpd.Click += new EventHandler(btUpd_Click);
+            this.Controls.Add(btUpd);
+
+            Button btDelete = new Button();
+            btDelete.Text = "Delete";
+            btDelete.Size = new Size(100, 30);
+            btDelete.Location = new Point(290, 330);
+            btDelete.Click += new EventHandler(btDelete_Click);
+            this.Controls.Add(btDelete);
+
+            Button btClose = new Button();
+            btClose.Text = "Return";
+            btClose.Size = new Size(100, 30);
+            btClose.Location = new Point(290, 330);
+            btClose.Click += new EventHandler(btClose_Click);
+            this.Controls.Add(btClose);
+        }
+    }
+}
